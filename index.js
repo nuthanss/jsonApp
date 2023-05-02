dcaf9e48a1f8a082313f1d0b191927cb9fff6c33
Nuthan S S <nss@icims.com>
2023-05-02 15:01:03 +0530

    added server and express for jwt auth

--- index.js
+++ index.js
@@ -1,15 +1,62 @@
-const jsonServer = require("json-server"); // importing json-server Library
-
+const express = require("express");
+const app = express();
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-
 const router = jsonServer.router("db.json");
+const jwt = require("jsonwebtoken");
 
-const middlewares = jsonServer.defaults();
+// Secret key used to sign JWT tokens
+const jwtSecret = "your_secret_key";
 
-const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3
+app.use(express.json());
+// Route for user authentication
+app.post("/login", (req, res) => {
+  console.log(req.body);
+  const { username, password } = req.body;
 
-server.use(middlewares);
+  // Get the users object from the JSON database
+  const users = router.db.get("users").value();
 
+  // Check if the user exists and the password is correct
+  if (users[username] && users[username].password === password) {
+    // Create a JWT token
+    const token = jwt.sign({ username }, jwtSecret);
+
+    // Send the token back to the client
+    res.json({ token });
+  } else {
+    res.status(401).send("Invalid credentials");
+  }
+});
+
+// Middleware function to authenticate requests
+const authenticate = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    res.status(401).send("Unauthorized");
+  } else {
+    const token = authHeader.split(" ")[1];
+    try {
+      const decodedToken = jwt.verify(token, jwtSecret);
+      req.user = decodedToken.username;
+      next();
+    } catch (error) {
+      res.status(401).send("Unauthorized");
+    }
+  }
+};
+
+// Add the authentication middleware to the JSON server
+server.use(authenticate);
 server.use(router);
 
-server.listen(port);
+// Start the server
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
+
+app.listen(3001, () => {
+  console.log("Express is listening on port 3001...");
+});
